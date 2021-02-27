7bd5f8028601866405e09e4d9a170dbdd8f23390
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-02-27 18:12:50 -0500

    commit tree 18dac58fac2894cf478705b71e00e908f9752e27

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,3 @@
-
-
 def unsafe?(speed)
     if speed < 40
         true
@@ -12,9 +10,9 @@ end
 
 
 
-#def not_safe?(speed)
-	60 < speed < 40 ? "true" : "false"
-#end
+def not_safe?(speed)
+	speed < 40 || speed > 60 ? true : false
+end
 	
 
 
